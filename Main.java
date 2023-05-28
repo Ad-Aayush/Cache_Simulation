e65be9bd984d1916a03f1f417bcd611643a6bbda
Aayush Adlakha <aayushadlakha15@gmail.com>
2023-05-29 01:04:15 +0530

    Mistakes removed

--- Main.java
+++ Main.java
@@ -2,102 +2,151 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
 
-public class Main {
-    public static void main(String args[]) {
+public
+class Main
+{
+public
+    static void main(String args[])
+    {
         if (args.length != 4)
             System.out.println(
-                    "INVALID FORMAT\nThe correct format is java Main.java Cache_size(in KB) Set_Associativity Block size(64B) PATH.");
-        else {
+                "INVALID FORMAT\nThe correct format is java Main.java Cache_size(in KB) Set_Associativity Block size(64B) PATH.");
+        else
+        {
             int cacheSize = 0, columnNum = 0, blockSize = 0;
             ArrayList<Block> fileHex = new ArrayList<Block>();
-            try {
+            try
+            {
                 cacheSize = Integer.parseInt(args[0]) * 1024;
-            } catch (NumberFormatException e) {
+            }
+            catch (NumberFormatException e)
+            {
                 System.out.println("Cache Size must be an Integer");
                 System.exit(0);
             }
-            try {
+            try
+            {
                 columnNum = Integer.parseInt(args[1]);
-            } catch (NumberFormatException e) {
+            }
+            catch (NumberFormatException e)
+            {
                 System.out.println("Associativity must be an Integer");
                 System.exit(0);
             }
-            try {
+            try
+            {
                 blockSize = Integer.parseInt(args[2]);
-            } catch (NumberFormatException e) {
+            }
+            catch (NumberFormatException e)
+            {
                 System.out.println("Block Size must be an Integer");
                 System.exit(0);
             }
             int rowNum = cacheSize / (blockSize * columnNum);
-            try {
+            try
+            {
                 File data = new File(args[3]);
                 Scanner myReader = new Scanner(data);
-                while (myReader.hasNextLong(16)) {
+                while (myReader.hasNextLong(16))
+                {
                     Long hex = myReader.nextLong(16);
                     String binString = (Long.toBinaryString(hex));
+                    if (binString.length() < 32)
+                    {
+                        int len = binString.length();
+                        for (int i = 0; i < 32 - len; i++)
+                        {
+                            binString = "0" + binString;
+                        }
+                    }
                     fileHex.add(new Block(binString, blockSize, rowNum));
                 }
                 myReader.close();
-            } catch (FileNotFoundException e) {
+            }
+            catch (FileNotFoundException e)
+            {
                 System.out.println("Invalid File Path");
                 System.exit(0);
             }
             ArrayList<ArrayList<Integer>> cache = new ArrayList<ArrayList<Integer>>();
-            for (int i = 0; i < rowNum; i++) {
+            for (int i = 0; i < rowNum; i++)
+            {
                 cache.add(new ArrayList<Integer>());
             }
             ArrayList<Integer> setHits = new ArrayList<Integer>();
             ArrayList<Integer> setMiss = new ArrayList<Integer>();
-            for (int i = 0; i < rowNum; i++) {
+            for (int i = 0; i < rowNum; i++)
+            {
                 setHits.add(0);
                 setMiss.add(0);
             }
-            for (Block current : fileHex) {
+            for (Block current : fileHex)
+            {
                 int setIndex = current.getSetIndex();
                 int tag = current.getTag();
                 ArrayList<Integer> row = cache.get(setIndex);
-                if (row.contains(tag)) {
+                if (row.contains(tag))
+                {
                     row.remove(row.indexOf(tag));
                     int currHits = setHits.get(setIndex);
                     setHits.set(setIndex, currHits + 1);
                     row.add(tag);
-                } else {
+                }
+                else
+                {
                     int currMiss = setMiss.get(setIndex);
                     setMiss.set(setIndex, currMiss + 1);
-                    if (row.size() < columnNum) {
+                    if (row.size() < columnNum)
+                    {
                         row.add(tag);
-                    } else {
+                    }
+                    else
+                    {
                         row.remove(0);
                         row.add(tag);
                     }
                 }
             }
             System.out.println("Set Index" + "\tSet Hits" + "\tSet Miss");
-            for (int i = 0; i < rowNum; i++) {
+            for (int i = 0; i < rowNum; i++)
+            {
                 System.out.println("    " + i + ":" + "\t\t   " + setHits.get(i) + "\t\t   " + setMiss.get(i));
             }
+            int totalHits = 0;
+            int totalMiss = 0;
+            for (int i = 0; i < rowNum; i++)
+            {
+                totalHits += setHits.get(i);
+                totalMiss += setMiss.get(i);
+            }
+            System.out.println("Total Hits: " + totalHits);
+            System.out.println("Total Misses: " + totalMiss);
         }
     }
 }
 
-class Block {
+class Block
+{
     int offset;
     int setIndex;
     int tag;
 
-    Block(String binary, int blockSize, int rowNum) {
-        int offsetBits = (int) (Math.log(blockSize) / Math.log(2));
-        int setIndexBits = (int) ((Math.log(rowNum) / Math.log(2)));
+    Block(String binary, int blockSize, int rowNum)
+    {
+        int offsetBits = (int)(Math.log(blockSize) / Math.log(2));
+        int setIndexBits = (int)((Math.log(rowNum) / Math.log(2)));
         this.offset = Integer.parseInt(binary.substring(32 - offsetBits), 2);
         this.setIndex = Integer.parseInt(binary.substring(32 - offsetBits - setIndexBits, 32 - offsetBits), 2);
         this.tag = Integer.parseInt(binary.substring(0, 32 - offsetBits - setIndexBits), 2);
     }
 
-    int getSetIndex() {
+    int getSetIndex()
+    {
         return this.setIndex;
     }
 
-    int getTag() {
+    int getTag()
+    {
         return this.tag;
     }
 }
